55e51272b5d4e9fd1bff241d5a16fd63435c95b5
Eli Gluch <eligluch@gmail.com>
2020-10-30 23:05:11 +0200

    Add video creation functionality.
    A clip will be created with all 6 question segments
    
    Signed-off-by: Eli Gluch <eligluch@gmail.com>

--- youtube.py
+++ youtube.py
@@ -3,12 +3,39 @@ import time
 import numpy as np
 from matplotlib import pyplot as plt
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-videoName = r"TheChase360.mp4"
 
+def createVideo(video, times):
+  if len(times) is not 6:
+    raise Exception("Error, expected exactly 6 times")
+
+  clips        = []
+
+  for i, startTime in enumerate(times):
+    # The first 4 rounds are one minute long, the fifth is 2, and the last is more than 2
+    endtime = startTime + 60 + 10
+
+    if i == 4:
+      endtime = startTime + 120 + 10
+
+    if i == 5:
+      endtime = None
+    
+    clip = VideoFileClip(video).subclip(t_start=startTime, t_end=endtime)
+    clips.append(clip)
+
+  final_clip = concatenate_videoclips(clips)
+  final_clip.write_videofile("HighlightsOf%s" % video)
+  
+
+lowQuality = True
+
+videoName = r"TheChase360.mp4" if lowQuality else r"TheChaseHigh.mp4"
+minutePic = r"minute360.png" if lowQuality else r"minute720.JPG"
 
 # Creates file called test1 consisting of 90 seconds from TheChase
-ffmpeg_extract_subclip(videoName, 10, 100, targetname="test1.mp4")
+# ffmpeg_extract_subclip(videoName, 10, 100, targetname="test1.mp4")
 
 def process_img(img_rgb, template, count):
 
@@ -24,10 +51,8 @@ def process_img(img_rgb, template, count):
     threshold = 0.8
     loc = np.where( res >= threshold)
     for pt in zip(*loc[::-1]):
-        print(' in ptt!!dagfdfgasdg')
-        print(pt)
+        print('Found frame')
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-        cv2.imwrite('res{0}.png'.format(count),img_rgb)   
         return True
     
     return False
@@ -40,29 +65,28 @@ def findTimes():
     # start frame numbers
     startFrames = []
 
-    vidcap = cv2.VideoCapture(videoName)
-    fps = vidcap.get(cv2.CAP_PROP_FPS)
-    print('fps is' + str(fps))
-    template = cv2.imread(r"minute360.png",0)  # open template only once
-    count = 0
+    template = cv2.imread(minutePic, 0)  # open template only once
+    vidcap   = cv2.VideoCapture(videoName)
+    fps      = vidcap.get(cv2.CAP_PROP_FPS)
+    count    = 0
+
     while True:
       success,image = vidcap.read()
       if not success: break         # loop and a half construct is useful
-    #   print ('Read a new frame: ', success)
       if count % 20 ==0:
         # Check only every 20 frames for match
         if process_img(image, template, count):
             startFrames.append(count)
 
             # If found start, skip ahead 2 minutes
-            for _ in range(3600):
+            for _ in range(int(fps * 130)):
                 count += 1
-                success,image = vidcap.read()
+                success,image = vidcap.read() 
                 if not success: break
 
       count += 1
     
-    startTimes = [frame/fps for frame in startFrames]
+    startTimes = [int(frame/fps) for frame in startFrames]
 
     return startTimes
     
@@ -74,6 +98,7 @@ def run():
     startTimes = findTimes()
     print('startTimes are:')
     print(startTimes)
+    createVideo(videoName, startTimes)
     print("--- %s seconds ---" % (time.time() - start_time))
 
 run()
